53601c71964013391345f60af6d1aed3267db58d
corbinarnett <corbin.c.arnett@gmail.com>
2019-12-11 22:43:09 -0800

    add seed data

--- job-hunter-api/db/seeds.rb
+++ job-hunter-api/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+List.create({title: "Portland"})
+
+Job.create({company:"Nike", position:"Software Engineer 1",
+  location:"Beaverton, OR", link:"https://www.linkedin.com/jobs/view/software-engineer-i-at-nike-1510852702/",
+  contacts:"zoe@swoosh.com", notes:"Looks like an awesome position", next_steps:"Send follow up email to Zoe", applied: true, list_id: 1
+  })
