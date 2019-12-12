9fee66fe7f633212defae16509e7a1b7e59824c1
corbinarnett <corbin.c.arnett@gmail.com>
2019-12-11 22:48:50 -0800

    add namespacing

--- job-hunter-api/config/routes.rb
+++ job-hunter-api/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :jobs
-  resources :lists
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do 
+    namespace :v1 do 
+      resources :jobs
+      resources :lists
+    end
+  end
 end
